3932d176bf6bcfcadcc439830e44c3a989b4f53c
kw2103 <yuuki.0821.0821.2005@gmail.com>
2024-12-13 16:43:18 +0900

    Delete RING.ipch

--- 1minutes/SourceCode/Player.h
+++ 1minutes/SourceCode/Player.h
@@ -1,1 +1,37 @@
 #pragma once
+
+#include "../GameLib/vector.h"
+
+
+#define PLAYER_TEX_W        (256.0f)   // プレイヤーの画像1つの幅
+#define PLAYER_TEX_H        (256.0f)   // プレイヤーの画像1つの高さ
+#define PLAYER_PIVOT_X      (PLAYER_TEX_W/2)    // プレイヤーの横方向の中心
+#define PLAYER_PIVOT_Y      (PLAYER_TEX_H/2)   // プレイヤーの縦方向の下端
+
+
+
+class PLAYER {
+public:
+	float angle;
+	VECTOR2 speed;
+	VECTOR2 position;
+	VECTOR2 scale;
+	VECTOR2 texPos;
+	VECTOR2 texSize;
+	VECTOR2 pivot;
+	VECTOR4 color;
+
+	int player_state;
+	float radius;
+	VECTOR2 offset;
+	float hp;
+
+	
+};
+
+
+
+void player_init();
+void player_deinit();
+void player_update();
+void player_render();
