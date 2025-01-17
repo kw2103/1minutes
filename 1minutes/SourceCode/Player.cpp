0d2edaa83d3def3ccc47414892fec73c4bb2ffec
kw2103 <yuuki.0821.0821.2005@gmail.com>
2025-01-17 14:59:47 +0900

    Update Player.cpp

--- 1minutes/SourceCode/Player.cpp
+++ 1minutes/SourceCode/Player.cpp
@@ -11,8 +11,6 @@ int shougai;//仮
 int nitoro;
 int nitoroCount;
 
-int grenade;
-int grenadeCount;
 
 float speedX;
 float speedY;
@@ -32,11 +30,27 @@ float BCX;
 float BCY;
 float B_angle;
 
+//グレラン
+int grenade;
+int grenadeCount;
+
+float GX;
+float GY;
+float GSX;
+float GSY;
+float GTX;
+float GTY;
+float GTW;
+float GTH;
+float GCX;
+float GCY;
+float G_angle;
+
 PLAYER player;
 
 Sprite* sprPlayer;
 Sprite* sprGrenade;
-Sprite* sprGun;
+Sprite* sprPlayerGun;
 Sprite* sprBullet;
 
 void player_init()
@@ -64,24 +78,29 @@ void player_update()
 	switch (player.state)
 	{
 	case 0:
-		sprPlayer  = sprite_load(L"./Data/Images/player.png");
-		sprGrenade = sprite_load(L"./Data/Images/Grenade.png");
-		sprGun     = sprite_load(L"./Data/Images/Gun.png");
-		sprBullet  = sprite_load(L"./Data/Images/Bullet.png");
+		sprPlayer    = sprite_load(L"./Data/Images/player.png");
+		sprGrenade   = sprite_load(L"./Data/Images/Grenade.png");
+		sprPlayerGun = sprite_load(L"./Data/Images/Gun.png");
+		sprBullet    = sprite_load(L"./Data/Images/Bullet.png");
 		++player.state;
 	case 1:
+		//player
 		player.angle = ToRadian(0);
 		player.scale = { 1.5f, 1.5f };
 		player.texPos = { 0, 0 };
 		player.texSize = { PLAYER_TEX_W, PLAYER_TEX_H };
 		player.pivot = { PLAYER_PIVOT_X, PLAYER_PIVOT_Y };
 		++player.state;
+
+		//
 	case 2:
 		//通常時
 		//プレイヤーのスピードが徐々に上がる
 		speedX++;
+
 		//プレイヤーが障害物に当たった時、減速する
 		speedX = 1.0f;
+
 		//スペースキーでニトロを使う
 		if (TRG(0) & PAD_TRG4)
 		{
@@ -91,10 +110,14 @@ void player_update()
 		//右クリックでグレランを使う
 		if (TRG(0) & PAD_R1)
 		{
+			GX++;
+			if (GX < 5.0f)
+			{
 
+				grenadeCount -= 1;
+			}
 		}
 		
-
 		++player.state;
 		
 	}
@@ -114,11 +137,14 @@ void player_render()
 		player.angle,
 		player.color.x, player.color.y, player.color.z, player.color.w
 	);
+
 	//左クリック長押ししてるときに弾丸を描画させる
+
 	if (STATE(0) & PAD_L1)
 	{
-		sprite_render(sprBullet, BX, BY, 
-			BSX, BSY, 
+		sprite_render(sprBullet, 
+			BX, BY, 
+			player.position.x, player.position.y, 
 			BTX, BTY, 
 			BTW, BTH, 
 			BCX, BCY, 
@@ -126,9 +152,16 @@ void player_render()
 			1.0f, 1.0f, 1.0f, 1.0f);
 	}
 	//右クリックを押したときにグレランを描写させる
+
 	if (TRG(0) & PAD_R1)
 	{
-		sprite_render(sprGrenade,);
+		sprite_render(sprGrenade,
+			GX,GY,
+			GTX,GTY,
+			GTW,GTH,
+			GCX,GCY,
+			1.0f,
+			1.0f,1.0f,1.0f,1.0f);
 	}
 	//機銃の描写（マウスカーソルを追わせる）
 
