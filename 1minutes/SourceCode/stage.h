4b22a4ad2c4f471d071f64c1246991b99ec01420
2240029 <2240029@ecc.ac.jp>
2025-01-18 13:47:46 +0900

    upload_stage.h

--- 1minutes/SourceCode/stage.h
+++ 1minutes/SourceCode/stage.h
@@ -1,10 +1,9 @@
 #ifndef STAGE_H
 #define STAGE_H
 
-#include "common.h"
-#include "obj2d.h"
+
 #include "enemy.h"
-#include "checkpoint.h"
+//#include "checkpoint.h"
 #include "../GameLib/game_lib.h"
 
 constexpr float STAGE_TEX_W = 400.0f;
@@ -14,28 +13,28 @@ constexpr float STAGE_TEX_H = 100.0f;
 
 class Stage
 {
-	Sprite* spr;
+	GameLib::Sprite* spr;
 	VECTOR2 pos;
 	int back_num;
 	int back_count;
 	int stage_num;
 	bool exsit;
 public:
-	Stage(int backNum ,int next_backNum,int backCount,int num, VECTOR2 backPos,Sprite* spr);
+	Stage(int backNum ,int next_backNum,int backCount,int num, VECTOR2 backPos,GameLib::Sprite* spr);
 	VECTOR2 stage_position();
 	bool stage_exsit();
 	void stage_move(VECTOR2 backPos, int backNum, VECTOR2 velocity);
 	bool stage_erase(int backNum, int backCount);
 	void stage_render(int backNum);
-	void stage_judge(OBJ2D* player);
-	void enemy_onStage(Enemy* enemy);
-	void checkPoint_through(CheckPoint* check, VECTOR2 back_pos, float player_posX);
+	//void stage_judge(OBJ2D* player);
+	//void enemy_onStage(Enemy* enemy);
+	//void checkPoint_through(CheckPoint* check, VECTOR2 back_pos, float player_posX);
 };
 
 class Stage_spr
 {
 public:
-	Sprite* spr[8];
+	GameLib::Sprite* spr[8];
 	Stage_spr();
 	void stage_load();
 	void stage_data_delete();
